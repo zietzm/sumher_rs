3799cc11ef4fd148f5bd4b425be767bc243370de
zietzm <michael.zietz@gmail.com>
2023-12-20 09:55:01 -0600

    Simplify build and fortran header

--- build.rs
+++ build.rs
@@ -1,9 +1,15 @@
 fn main() {
+    println!("cargo:rerun-if-changed=src/ldak/sumfuns.h");
+    println!("cargo:rerun-if-changed=src/ldak/sumfuns.c");
+    println!("cargo:rerun-if-changed=src/ldak/decomp.h");
+    println!("cargo:rerun-if-changed=src/ldak/decomp.c");
+    println!("cargo:rerun-if-changed=src/ldak/fortran_functions.h");
+
     cc::Build::new()
         .file("src/ldak/sumfuns.c")
         .file("src/ldak/decomp.c")
         .compile("ldak");
 
-    println!(r"cargo:rustc-link-lib=static=lapack");
-    println!(r"cargo:rustc-link-lib=static=blas");
+    println!(r"cargo:rustc-link-lib=lapack");
+    println!(r"cargo:rustc-link-lib=blas");
 }
